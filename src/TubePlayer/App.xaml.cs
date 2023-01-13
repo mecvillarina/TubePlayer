c1b6e8dfd3ad2ae6ffc66c491755dbe5882181a4
Mark Erwin C. Villarina <me.villarina@live.com>
2023-01-13 11:47:08 +0800

    Added Global Usings

--- src/TubePlayer/App.xaml.cs
+++ src/TubePlayer/App.xaml.cs
@@ -1,11 +1,33 @@
-﻿namespace TubePlayer;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace TubePlayer;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    const int WindowWidth = 540;
+    const int WindowHeight = 1000;
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        VersionTracking.Track();
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+#if WINDOWS
+			var mauiWindows = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandler = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandler);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+#endif
+        });
+        MainPage = new AppShell();
+    }
 }
